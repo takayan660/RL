a1d6d5939cc933db5df91e3f000c87a3dacb7a5a
takayan660 <s1526084PQ@s.chibakoudai.jp>
2017-07-12 08:17:29 +0900

    Changing processing method

--- gym/MountainCar-v0_Q.py
+++ gym/MountainCar-v0_Q.py
@@ -1,27 +1,40 @@
 import gym
-from gym import envs
+from gym import wrappers
 import numpy as np
 import sys
 
-state1p_num = 180        # 状態数
-state2v_num = 140
+state_num = 40          # 状態数
 action_num = 3          # 行動数
-epiode_num = 1000000    # 評価を行うエピソード数
+epiode_num = 10000      # 評価を行うエピソード数
 step = 1000
-policySelect = 0        # 0: e-greedy手法 1: softmax手法
-alpha = 0.7
-gamma = 0.9
-tau = 0.0016            # softmax手法の温度
+policySelect = 1        # 0: e-greedy手法 1: softmax手法
+alpha = 1.0
+gamma = 1.0
+tau = 0.04              # softmax手法の温度
 goal = 0                # 最後の100回中ゴールした回数
 goal_total = 0
+
+def run_episode(env, Q, render=False):
+    state = env.reset()
+    eps = 0.02
+    for _ in range(200):
+        if render:
+            env.render()
+        # 行動aの獲得
+        # action = env.action_space.sample()
+        action = Ql.choose_action(Q, state, eps)
+
+        # 実行
+        observation, reward, done, _ = env.step(action)
+
 
 class Qlearning:
     """ class for Q Learning """
     @classmethod
-    def choose_action(self, Q, new_state, epsilon):
+    def choose_action(self, Q, new_state, eps):
         """ 行動決定 """
         if policySelect == 0: # e-greedy
-            if epsilon < np.random.uniform():
+            if eps < np.random.uniform():
                 # greedy法を適用する
                 return np.argmax(Q[int(new_state[0]), int(new_state[1]),:])
             else:
@@ -31,13 +44,16 @@ class Qlearning:
             policy = np.exp(Q[int(new_state[0]), int(new_state[1]),:]/tau) / np.sum(np.exp(Q[int(new_state[0]), int(new_state[1]),:]/tau))
             
             # 逆関数法で行動選択
-            random = np.random.uniform()
+            #random = np.random.uniform()
+            random = 1
             cprob = 0
             for a in range(action_num):
                 cprob = cprob + policy[a]
                 action = a
-                if random < cprob:
+                if random*10 < cprob:
                     break
+
+            #action = np.random.choice(env.action_space.n, p=policy)
 
             return action
 
@@ -48,34 +64,47 @@ class Qlearning:
 
 if __name__=='__main__':
     Ql = Qlearning
-    Q = np.zeros([state1p_num, state2v_num, action_num])
-    epsilon = 1.0
+    Q = np.zeros([state_num, state_num, action_num])
+    eps = 0.8
+    new_state = np.zeros(2)
+    reward_observation = np.zeros(2)
 
     env = gym.make('MountainCar-v0')
+    #env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
+
+    env_low = env.observation_space.low
+    env_high = env.observation_space.high
+    env_dx = (env_high - env_low) / state_num
 
     for i_episode in range(epiode_num):
         state = env.reset()
-        epsilon *= 0.9999
-        if epsilon < 1e-3:
-            epsilon = 0.001
+        eps *= 0.999
+        if eps < 4e-2:
+            eps = 0.04
         print("Episode {}".format(i_episode+1))
         for t in range(step):
-            env.render()
-
             # 行動aの獲得
             # action = env.action_space.sample()
-            action = Ql.choose_action(Q, state, epsilon)
+            action = Ql.choose_action(Q, state, eps)
 
             # 実行
-            new_state, _, done, _ = env.step(action)
+            observation, reward, done, _ = env.step(action)
 
             # reward の獲得
-            reward = 1 / (1+(0.5-new_state[0])**2)
+            # reward = 0.01 / (0.01+(env_high[1]-abs(new_state[1]))**2)
 
             # 離散化
-            new_state[0] = round(new_state[0]*100)+120
-            new_state[1] = round(new_state[1]*100)+70
+            # new_state[0] = round(new_state[0]*100)+120
+            # new_state[1] = round(new_state[1]*100)+70
+            new_state[0] = int((observation[0] - env_low[0])/env_dx[0])
+            new_state[1] = int((observation[1] - env_low[1])/env_dx[1])
             
+            # reward の獲得
+            reward_observation[0] = new_state[0]
+            reward_observation[1] = int((abs(observation[1]) - env_low[1])/env_dx[1])
+            state_ave = np.sum(reward_observation)/2
+            reward = 10 / (10+(state_num-state_ave)**2)
+
             # Q学習の更新
             Q[int(state[0]), int(state[1]), action] = Ql.Qupdate(Q, new_state, state, action, reward)
 
@@ -86,10 +115,9 @@ if __name__=='__main__':
             # 状態と行動の記録
             state = new_state
 
-            print new_state[0]
-
             if done:
-                if new_state[0] == state1p_num-10:
+                print eps
+                if new_state[0] == state_num-10:
                     goal_total += 1
                     if i_episode > epiode_num-101:
                         goal += 1
@@ -97,9 +125,12 @@ if __name__=='__main__':
                 print("Episode finished after {} timesteps".format(t+1))
                 break
 
+    run_episode(env, Q, True)
+
     print("Q(s,a) =\n{}".format(Q))
     print("Goal total: {}".format(goal_total))
     print("{}/100".format(goal))
-    print(epsilon)
+    print(eps)
 
     env.close()
+    #gym.upload('/tmp/cartpole-experiment-1', api_key='sk_H98pDlIsRJWR9A3QmoKHhQ')
